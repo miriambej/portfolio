1340adb9b45d1806020326d55beb55254e67806e
miriambej <justme0112@hotmail.com>
2017-10-22 02:44:26 +1100

    change more features to green

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-    // Only process POST requests.
+    // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
